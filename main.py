10e82c17680c049d029132f8b57b2a21ce0051d1
Your Name <you@example.com>
2026-03-27 09:34:11 -0300

    Teste

---
+++ main.py
@@ -0,0 +1,36 @@
+print("Olá Mundo")
+
+nota01 = 46
+nota02 = 10
+nota03 = 9
+nota04 = 26
+
+media = (nota01 + nota02 + nota03 + nota04) / 4
+
+print("A média das nota é:", media)
+
+print("A multiplicação é: ", nota01 * nota03)
+
+# Variáveis
+idade = 25
+nome = "João"
+altura = 1.75
+
+#Exibir dados
+print("Nome:", nome)
+print("Idade:", idade)
+print("altura:", altura)
+
+idade = 26
+
+print("##########################")
+#Exibir dados
+print("Nome:", nome)
+print("Idade:", idade)
+print("altura:", altura)
+
+print("##########################")
+if idade >= 18:
+    print(nome, "tem maior idade")
+else: 
+    print(nome, "tem menor idade")
